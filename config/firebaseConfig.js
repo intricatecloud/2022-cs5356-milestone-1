2667f8efe487414e652e22778e6d841a24deee10
Daan van der Zwaag <daan@devign.it>
2022-01-26 19:03:08 -0500

    feat: add serviceAccount

---
+++ config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfigSecure = {
+    apiKey: "AIzaSyBF4BxY_1MgepA3J5aHP2z88f8ftjH6gwU",
+    authDomain: "cs5356-22.firebaseapp.com",
+    projectId: "cs5356-22",
+    storageBucket: "cs5356-22.appspot.com",
+    messagingSenderId: "802452725516",
+    appId: "1:802452725516:web:798e1cffa572327083b329",
+    measurementId: "G-PK7ELW8TLH"
+  };
