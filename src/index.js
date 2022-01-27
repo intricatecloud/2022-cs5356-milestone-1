9a755f8ff6efa9853465d7ce75d4427d33ab58b8
Daan van der Zwaag <daan@devign.it>
2022-01-26 23:10:05 -0500

    feat: set sessionLogin cookie and csrfToken

--- src/index.js
+++ src/index.js
@@ -15,6 +15,38 @@ const serviceAccount = require("./../config/serviceAccountKey.json");
 const userFeed = require("./app/user-feed");
 const authMiddleware = require("./app/auth-middleware");
 
+// Helper functions to attach CSRF Token
+function attachCsrfToken(url, cookie, value) {
+  return function (req, res, next) {
+    if (req.url == url) {
+      res.cookie(cookie, value);
+    }
+    next();
+  };
+}
+
+// Helper functions to check if user is signed In
+
+function checkIfSignedIn(url) {
+  return function (req, res, next) {
+    if (req.url == url) {
+      const sessionCookie = req.cookies.session || "";
+      // User already logged in. Redirect to profile page.
+      admin
+        .auth()
+        .verifySessionCookie(sessionCookie)
+        .then(function (decodedClaims) {
+          res.redirect("/profile");
+        })
+        .catch(function (error) {
+          next();
+        });
+    } else {
+      next();
+    }
+  };
+}
+
 // DONE ✅
 
 // CS5356 TODO #2
@@ -31,6 +63,18 @@ app.use(
     extended: true,
   })
 );
+
+// Attach CSRF token on each request.
+app.use(
+  attachCsrfToken(
+    "/",
+    "csrfToken",
+    (Math.random() * 100000000000000000).toString()
+  )
+);
+// If a user is signed in, redirect to profile page.
+app.use(checkIfSignedIn("/"));
+
 // set the view engine to ejs
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -61,8 +105,47 @@ app.post("/sessionLogin", async (req, res) => {
   // Get the ID token from the request body
   // Create a session cookie using the Firebase Admin SDK
   // Set that cookie with the name 'session'
-  // And then return a 200 status code instead of a 501
-  res.status(501).send();
+
+  // Get ID token and CSRF token.
+  const idToken = req.body.idToken.toString();
+  const csrfToken = req.body.csrfToken.toString();
+
+  // Guard against CSRF attacks.
+  if (!req.cookies || csrfToken !== req.cookies.csrfToken) {
+    res.status(401).send("UNAUTHORIZED REQUEST!");
+    return;
+  }
+  // Set session expiration to 5 days.
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+  // Create the session cookie. This will also verify the ID token in the process.
+  // The session cookie will have the same claims as the ID token.
+  // We could also choose to enforce that the ID token auth_time is recent.
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(function (decodedClaims) {
+      // In this case, we are enforcing that the user signed in in the last 5 minutes.
+      if (new Date().getTime() / 1000 - decodedClaims.auth_time < 5 * 60) {
+        return admin
+          .auth()
+          .createSessionCookie(idToken, { expiresIn: expiresIn });
+      }
+      throw new Error("UNAUTHORIZED REQUEST!");
+    })
+    .then(function (sessionCookie) {
+      // Note httpOnly cookie will not be accessible from javascript.
+      // secure flag should be set to true in production.
+      const options = {
+        maxAge: expiresIn,
+        httpOnly: true,
+        secure: false /** to test in localhost */,
+      };
+      res.cookie("session", sessionCookie, options);
+      res.end(JSON.stringify({ status: "success" }));
+    })
+    .catch(function (error) {
+      res.status(401).send("UNAUTHORIZED REQUEST!");
+    });
 });
 
 app.get("/sessionLogout", (req, res) => {
