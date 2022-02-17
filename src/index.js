7e31ce5e236dc4adc6850a10c68f60051d2544e4
BasiqueSimple <romainfaures@hotmail.com>
2022-02-17 00:16:12 -0500

    milestone1 v1.0 DONE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// Express is a framework that allows you to reuse pieces of HTML and
+// to manage the HTTP requests.
+// This is how you import packages in JavaScript:
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -6,18 +9,14 @@ const admin = require("firebase-admin");
 const app = express();
 const port = process.env.PORT || 8080;
 
-// CS5356 TODO #2
-// Uncomment this next line after you've created
-// serviceAccountKey.json
-// const serviceAccount = require("./../config/serviceAccountKey.json");
+const serviceAccount = require("../serviceAccountKey.json");
 const userFeed = require("./app/user-feed");
 const authMiddleware = require("./app/auth-middleware");
 
-// CS5356 TODO #2
-// Uncomment this next block after you've created serviceAccountKey.json
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // use cookies
 app.use(cookieParser());
@@ -27,6 +26,7 @@ app.use(
     extended: true,
   })
 );
+
 // set the view engine to ejs
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -35,6 +35,8 @@ app.use("/static", express.static("static/"));
 
 // use res.render to load up an ejs view file
 // index page
+// This handles the GET request with "/" url, it asks the view engine to render
+// the view "pages/index".
 app.get("/", function (req, res) {
   res.render("pages/index");
 });
@@ -53,24 +55,42 @@ app.get("/dashboard", authMiddleware, async function (req, res) {
 });
 
 app.post("/sessionLogin", async (req, res) => {
-  // CS5356 TODO #4
-  // Get the ID token from the request body
-  // Create a session cookie using the Firebase Admin SDK
-  // Set that cookie with the name 'session'
-  // And then return a 200 status code instead of a 501
-  res.status(501).send();
+  // Get the ID token passed.
+  const idToken = req.body.idToken.toString();
+
+  // Set session expiration to 5 days.
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+  // Create the session cookie. This will also verify the ID token in the process.
+  // The session cookie will have the same claims as the ID token.
+  // To only allow session cookie setting on recent sign-in, auth_time in ID token
+  // can be checked to ensure user was recently signed in before creating a session cookie.
+  const adminAuth = require("firebase-admin/auth");
+  adminAuth.getAuth()
+    .createSessionCookie(idToken, { expiresIn })
+    .then(
+      (sessionCookie) => {
+        // Set cookie policy for session cookie.
+        const options = { maxAge: expiresIn, httpOnly: true, secure: true };
+        res.cookie('session', sessionCookie, options);
+        res.status(200).send();
+      }).catch((error) => {
+        console.error(error)
+        res.status(401).send('UNAUTHORIZED REQUEST!');
+      }
+    );
 });
 
 app.get("/sessionLogout", (req, res) => {
   res.clearCookie("session");
-  res.redirect("/sign-in");
+  res.redirect("/");
 });
 
 app.post("/dog-messages", authMiddleware, async (req, res) => {
-  // CS5356 TODO #5
-  // Get the message that was submitted from the request body
-  // Get the user object from the request body
-  // Add the message to the userFeed so its associated with the user
+  const msg = req.body.message
+  const user = req.user
+  await userFeed.add(user, msg)
+  res.redirect("/dashboard")
 });
 
 app.listen(port);
