2b73bf70108dc8d969309261d4d6aaff6cab0e5a
Yichao Cui <yc793@cornell.edu>
2022-02-19 13:22:34 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+// import firebase from 'firebase/compat/app';
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+//
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC-PTJnNJneTbtgJSo8A2gFt5XIx1J6BdA",
+//   authDomain: "ruffhouse-f536e.firebaseapp.com",
+//   projectId: "ruffhouse-f536e",
+//   storageBucket: "ruffhouse-f536e.appspot.com",
+//   messagingSenderId: "448119311869",
+//   appId: "1:448119311869:web:faceb106f6a09239fadd94",
+//   measurementId: "G-FYRL4N2MGQ"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -9,15 +31,15 @@ const port = process.env.PORT || 8080;
 // CS5356 TODO #2
 // Uncomment this next line after you've created
 // serviceAccountKey.json
-// const serviceAccount = require("./../config/serviceAccountKey.json");
+const serviceAccount = require("./../config/serviceAccountKey.json");
 const userFeed = require("./app/user-feed");
 const authMiddleware = require("./app/auth-middleware");
 
 // CS5356 TODO #2
 // Uncomment this next block after you've created serviceAccountKey.json
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // use cookies
 app.use(cookieParser());
@@ -35,21 +57,24 @@ app.use("/static", express.static("static/"));
 
 // use res.render to load up an ejs view file
 // index page
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.render("pages/index");
 });
 
-app.get("/sign-in", function (req, res) {
+app.get("/sign-in", function(req, res) {
   res.render("pages/sign-in");
 });
 
-app.get("/sign-up", function (req, res) {
+app.get("/sign-up", function(req, res) {
   res.render("pages/sign-up");
 });
 
-app.get("/dashboard", authMiddleware, async function (req, res) {
+app.get("/dashboard", authMiddleware, async function(req, res) {
   const feed = await userFeed.get();
-  res.render("pages/dashboard", { user: req.user, feed });
+  res.render("pages/dashboard", {
+    user: req.user,
+    feed
+  });
 });
 
 app.post("/sessionLogin", async (req, res) => {
@@ -58,7 +83,30 @@ app.post("/sessionLogin", async (req, res) => {
   // Create a session cookie using the Firebase Admin SDK
   // Set that cookie with the name 'session'
   // And then return a 200 status code instead of a 501
-  res.status(501).send();
+  console.log("body", req.body)
+  const idToken = req.body.idToken.toString();
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+  admin.auth().createSessionCookie(idToken, {
+      expiresIn
+    })
+    .then(
+      (sessionCookie) => {
+        // Set cookie policy for sescssion cookie.
+        const options = {
+          maxAge: expiresIn,
+          httpOnly: true,
+          secure: true
+        };
+        res.cookie('session', sessionCookie, options);
+        res.status(200).send(JSON.stringify({
+          status: 'success'
+        }));
+      },
+      (error) => {
+        res.status(401).send('UNAUTHORIZED REQUEST!');
+      }
+    );
+
 });
 
 app.get("/sessionLogout", (req, res) => {
@@ -71,6 +119,15 @@ app.post("/dog-messages", authMiddleware, async (req, res) => {
   // Get the message that was submitted from the request body
   // Get the user object from the request body
   // Add the message to the userFeed so its associated with the user
+  // console.log("dog messages!!!")
+  const message = req.body.message
+  const user = req.user
+  await userFeed.add(user, message)
+  const feed = await userFeed.get();
+  res.render("pages/dashboard", {
+    user: req.user,
+    feed
+  });
 });
 
 app.listen(port);
