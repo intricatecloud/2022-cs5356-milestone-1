7df40b958f169876079c7a531d4c698604eac14e
xingyu tao <taoxingyu@gmail.com>
2022-02-20 22:31:40 -0500

    finished tasks 3 and 4

--- src/index.js
+++ src/index.js
@@ -46,12 +46,20 @@ app.get("/dashboard", authMiddleware, async function (req, res) {
 });
 
 app.post("/sessionLogin", async (req, res) => {
-  // CS5356 TODO #4
-  // Get the ID token from the request body
-  // Create a session cookie using the Firebase Admin SDK
-  // Set that cookie with the name 'session'
-  // And then return a 200 status code instead of a 501
-  res.status(501).send();
+  const idToken = req.body.idToken
+  const expiresIn = 60 * 60 * 24 * 5 * 1000
+  
+  admin.auth().createSessionCookie(idToken, { expiresIn })
+    .then(
+      sessionCookie => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.status(200).send(JSON.stringify({ status: "success" }));
+      },
+      error => {
+        res.status(401).send("UNAUTHORIZED REQUEST!");
+      }
+    );
 });
 
 app.get("/sessionLogout", (req, res) => {
