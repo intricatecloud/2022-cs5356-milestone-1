34002d87e16b3741f6dce01e06d967b49e1b3327
Sarang-Pramode <sarangpramode1997@gmail.com>
2022-02-21 22:11:15 -0500

    basic text on landing ,  sign up basic functionality with firebase, made sign up button visible, sign in not working at all

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ const port = process.env.PORT || 8080;
 // CS5356 TODO #2
 // Uncomment this next line after you've created
 // serviceAccountKey.json
-// const serviceAccount = require("./../config/serviceAccountKey.json");
+const serviceAccount = require("serviceAccountKey.json");
 const userFeed = require("./app/user-feed");
 const authMiddleware = require("./app/auth-middleware");
 
 // CS5356 TODO #2
 // Uncomment this next block after you've created serviceAccountKey.json
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // use cookies
 app.use(cookieParser());
@@ -58,7 +58,8 @@ app.post("/sessionLogin", async (req, res) => {
   // Create a session cookie using the Firebase Admin SDK
   // Set that cookie with the name 'session'
   // And then return a 200 status code instead of a 501
-  res.status(501).send();
+  const idToken = req.body;
+  // res.sendStatus(501)
 });
 
 app.get("/sessionLogout", (req, res) => {
@@ -72,6 +73,7 @@ app.post("/dog-messages", authMiddleware, async (req, res) => {
   // Get the user object from the request body
   // Add the message to the userFeed so its associated with the user
 });
+
 
 app.listen(port);
 console.log("Server started at http://localhost:" + port);
