0d777c45f315f407de0f671b630212f5ed145ed7
SalGalarza <46201043+SalGalarza@users.noreply.github.com>
2022-02-23 14:47:00 -0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -9,15 +9,16 @@ const port = process.env.PORT || 8080;
 // CS5356 TODO #2
 // Uncomment this next line after you've created
 // serviceAccountKey.json
+const serviceAccount = require("/Users/salgalarza/BuildingSS/2022-cs5356-milestone-1/src/serviceAccountKey.json");
 // const serviceAccount = require("./../config/serviceAccountKey.json");
 const userFeed = require("./app/user-feed");
 const authMiddleware = require("./app/auth-middleware");
 
 // CS5356 TODO #2
 // Uncomment this next block after you've created serviceAccountKey.json
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // use cookies
 app.use(cookieParser());
@@ -48,17 +49,55 @@ app.get("/sign-up", function (req, res) {
 });
 
 app.get("/dashboard", authMiddleware, async function (req, res) {
+
+  // const sessionCookie = req.cookies.session || '';
+  // Verify the session cookie. In this case an additional check is added to detect
+  // if the user's Firebase session was revoked, user deleted/disabled, etc.
   const feed = await userFeed.get();
   res.render("pages/dashboard", { user: req.user, feed });
+
+  // getAuth()
+  //   .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+  //   .then((decodedClaims) => {
+  //     res.render("pages/dashboard", { user: req.user, feed });
+  //   })
+  //   .catch((error) => {
+  //     // Session cookie is unavailable or invalid. Force user to login.
+  //     res.redirect('/login');
+  //   });
+
+  // const feed = await userFeed.get();
+  // res.render("pages/dashboard", { user: req.user, feed });
+
 });
 
 app.post("/sessionLogin", async (req, res) => {
   // CS5356 TODO #4
   // Get the ID token from the request body
+  // const body = req.body;
+  // const requestBodyAsObject = JSON.parse(body)
+  const idToken = req.body.idToken;
+
+  const expiresIn = 60*60*24*5*1000;
+
+  admin
+    .auth()
+    .createSessionCookie(idToken, {expiresIn})
+    .then(
+      sessionCookie => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.status(200).send(JSON.stringify({ status: "success" }));
+      },
+      error => {
+        res.status(401).send("UNAUTHORIZED REQUEST!");
+      }
+    );
+
   // Create a session cookie using the Firebase Admin SDK
   // Set that cookie with the name 'session'
   // And then return a 200 status code instead of a 501
-  res.status(501).send();
+  // res.status(501).send();
 });
 
 app.get("/sessionLogout", (req, res) => {
@@ -71,6 +110,14 @@ app.post("/dog-messages", authMiddleware, async (req, res) => {
   // Get the message that was submitted from the request body
   // Get the user object from the request body
   // Add the message to the userFeed so its associated with the user
+  try {
+    const dogMessage = req.body;
+
+    await userFeed.add(req.user, dogMessage.message);
+    res.redirect("/dashboard");
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
 });
 
 app.listen(port);
