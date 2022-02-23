ace5cc6c4fd57a09a98eea89da7efe489a1ed446
Jeanne Li <xl863@cornell.edu>
2022-02-23 10:01:02 -0500

    2

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ const port = process.env.PORT || 8080;
 // CS5356 TODO #2
 // Uncomment this next line after you've created
 // serviceAccountKey.json
-const serviceAccount = require("./../config/serviceAccountKey.json");
+// const serviceAccount = require("./../config/serviceAccountKey.json");
 const userFeed = require("./app/user-feed");
 const authMiddleware = require("./app/auth-middleware");
 
 // CS5356 TODO #2
 // Uncomment this next block after you've created serviceAccountKey.json
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
 // use cookies
 app.use(cookieParser());
@@ -90,8 +90,8 @@ app.post("/dog-messages", authMiddleware, async (req, res) => {
   // Get the user object from the request body
   // Add the message to the userFeed so its associated with the user
   try {
-    const petMessage = req.body;
-    userFeed.add(req.user, petMessage.message)
+    const Message = req.body;
+    userFeed.add(req.user, Message.message)
       .then(() => {
         res.redirect('/dashboard');
       })
