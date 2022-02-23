581b1ec411ce324f7fa8dc4fbaf5c07d365f6100
mac545 <44330592+mac545@users.noreply.github.com>
2022-02-23 17:31:48 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ const admin = require("firebase-admin");
 const app = express();
 const port = process.env.PORT || 8080;
 
-// CS5356 TODO #2
+// CS5356 DONE #2
 // Uncomment this next line after you've created
 // serviceAccountKey.json
-// const serviceAccount = require("./../config/serviceAccountKey.json");
+const serviceAccount = require("./../config/serviceAccountKey.json");
 const userFeed = require("./app/user-feed");
 const authMiddleware = require("./app/auth-middleware");
 
-// CS5356 TODO #2
+// CS5356 DONE #2
 // Uncomment this next block after you've created serviceAccountKey.json
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount),
+ });
 
 // use cookies
 app.use(cookieParser());
@@ -53,17 +53,41 @@ app.get("/dashboard", authMiddleware, async function (req, res) {
 });
 
 app.post("/sessionLogin", async (req, res) => {
-  // CS5356 TODO #4
+  // CS5356 DONE #4
   // Get the ID token from the request body
   // Create a session cookie using the Firebase Admin SDK
   // Set that cookie with the name 'session'
   // And then return a 200 status code instead of a 501
+  if(req.body){
+    const idToken = req.body.id;
+    // Set session expiration to 5 days.
+    const expiresIn = 60 * 60 * 24 * 5 * 1000;
+    // Create the session cookie. This will also verify the ID token in the process.
+    // The session cookie will have the same claims as the ID token.
+    // To only allow session cookie setting on recent sign-in, auth_time in ID token
+    // can be checked to ensure user was recently signed in before creating a session cookie.
+    admin.auth()
+      .createSessionCookie(idToken, { expiresIn })
+      .then(
+        (sessionCookie) => {
+          // Set cookie policy for session cookie.
+          const options = { maxAge: expiresIn, httpOnly: true, secure: true };
+          res.cookie('session', sessionCookie, options);
+          res.status(200).send();
+        },
+        (error) => {
+          res.status(401).send('UNAUTHORIZED REQUEST!');
+        }
+      );
+  }else{
   res.status(501).send();
+  }
 });
 
 app.get("/sessionLogout", (req, res) => {
   res.clearCookie("session");
   res.redirect("/sign-in");
+  console.log("Logged out");
 });
 
 app.post("/dog-messages", authMiddleware, async (req, res) => {
@@ -71,6 +95,13 @@ app.post("/dog-messages", authMiddleware, async (req, res) => {
   // Get the message that was submitted from the request body
   // Get the user object from the request body
   // Add the message to the userFeed so its associated with the user
+  
+  var user = {
+    name: req.user.email.substring(0, req.user.email.indexOf('@')),
+    email: req.user.email
+  };
+  await userFeed.add(user, req.body.message);
+  res.redirect("/dashboard");
 });
 
 app.listen(port);
