5b2e8961458fd78a2e3636fc06d36653f32dddd5
scw89 <98471735+scw89@users.noreply.github.com>
2022-02-23 19:04:24 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -9,15 +9,16 @@ const port = process.env.PORT || 8080;
 // CS5356 TODO #2
 // Uncomment this next line after you've created
 // serviceAccountKey.json
-// const serviceAccount = require("./../config/serviceAccountKey.json");
+const serviceAccount = require("./jsonconfig/serviceAccountKey.json");
 const userFeed = require("./app/user-feed");
 const authMiddleware = require("./app/auth-middleware");
+const exp = require("constants");
 
 // CS5356 TODO #2
 // Uncomment this next block after you've created serviceAccountKey.json
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // use cookies
 app.use(cookieParser());
@@ -58,7 +59,23 @@ app.post("/sessionLogin", async (req, res) => {
   // Create a session cookie using the Firebase Admin SDK
   // Set that cookie with the name 'session'
   // And then return a 200 status code instead of a 501
-  res.status(501).send();
+
+  const idToken = req.body.idToken;
+  const expiresIn = 60*60*24*5*1000
+
+  admin 
+    .auth()
+    .createSessionCookie(idToken, {expiresIn})
+    .then(
+      sessionCookie => {
+        const options = { maxAge: expiresIn, httpOnly: true};
+        res.cookie("session", sessionCookie, options);
+        res.status(200).send(JSON.stringify({status:"success"}));
+      },
+      error => {
+        res.status(501).send("unauthorized request")
+      }
+    )
 });
 
 app.get("/sessionLogout", (req, res) => {
@@ -71,6 +88,14 @@ app.post("/dog-messages", authMiddleware, async (req, res) => {
   // Get the message that was submitted from the request body
   // Get the user object from the request body
   // Add the message to the userFeed so its associated with the user
+  try {
+    const dogMessage = req.body;
+
+    await userFeed.add(req.user, dogMessage.message);
+    res.redirect("/dashboard");
+  } catch(err){
+    res.status(500).send({message:err});
+  }
 });
 
 app.listen(port);
