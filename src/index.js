e86032aaf27dff383dcd338cc097a636862aacf4
JamesAn548 <88353322+JamesAn548@users.noreply.github.com>
2022-04-23 16:52:29 -0400

    api_tested

--- src/index.js
+++ src/index.js
@@ -72,7 +72,6 @@ app.delete('/delete', async function(req, response) {
     await client.connect();
     const collection = client.db("game").collection("scores");
 
-    // check if the username already exists
     const alreadyExisting = await collection.findOne({ username: req.body.username });
     if (alreadyExisting) {
         const result = await collection.deleteMany(alreadyExisting);
@@ -84,108 +83,6 @@ app.delete('/delete', async function(req, response) {
     }
     
 });
-
-
-// // The index route
-// app.get('/', function(req, res) {
-//    res.send('Sweet Game Leaderboard API!');
-// });
-
-// // Connect to the database with [url]
-// (async () => {
-//    let client = await MongoClient.connect(
-//        url,
-//        { useNewUrlParser: true }
-//    );
-
-//    db = client.db('Players');
-
-//    app.listen(PORT, async function() {
-//        console.log(`Listening on Port ${PORT}`);
-//        if (db) {
-//            console.log('Database is Connected!');
-//        }
-//    });
-// })();
-
-
-// // Route to create new player
-// app.post('/players', async function(req, res) {
-//   // get information of player from POST body data
-//   let { username, score } = req.body;
-
-//   // check if the username already exists
-//   const alreadyExisting = await db
-//       .collection('players')
-//       .findOne({ username: username });
-
-//   if (alreadyExisting) {
-//       res.send({ status: false, msg: 'player username already exists' });
-//   } else {
-//       // create the new player
-//       await db.collection('players').insertOne({ username, score });
-//       console.log(`Created Player ${username}`);
-//       res.send({ status: true, msg: 'player created' });
-//   }
-// });
-
-
-// app.put('/players', async function(req, res) {
-//   let { username, score } = req.body;
-//   // check if the username already exists
-//   const alreadyExisting = await db
-//       .collection('players')
-//       .findOne({ username: username });
-//   if (alreadyExisting) {
-//       // Update player object with the username
-//       await db
-//           .collection('players')
-//           .updateOne({ username }, { $set: { username, score } });
-//       console.log(`Player ${username} score updated to ${score}`);
-//       res.send({ status: true, msg: 'player score updated' });
-//   } else {
-//       res.send({ status: false, msg: 'player username not found' });
-//   }
-// });
-
-
-// // delete player
-// app.delete('/players', async function(req, res) {
-//   let { username, score } = req.body;
-//   // check if the username already exists
-//   const alreadyExisting = await db
-//       .collection('players')
-//       .findOne({ username: username });
-
-//   if (alreadyExisting) {
-//       await db.collection('players').deleteOne({ username });
-//       console.log(`Player ${username} deleted`);
-//       res.send({ status: true, msg: 'player deleted' });
-//   } else {
-//       res.send({ status: false, msg: 'username not found' });
-//   }
-// });
-
-// // Access the leaderboard
-// app.get('/players', async function(req, res) {
-//   // retrieve ‘lim’ from the query string info
-//   let { lim } = req.query;
-//   db.collection('players')
-//       .find()
-//       // -1 is for descending and 1 is for ascending
-//       .sort({ score: -1 })
-//       // Show only [lim] players
-//       .limit(lim)
-//       .toArray(function(err, result) {
-//           if (err)
-//               res.send({ status: false, msg: 'failed to retrieve players' });
-//           console.log(Array.from(result));
-//           res.send({ status: true, msg: result });
-//       });
-// });
-
-
-// Connect to the database with the url
 
 
 // CS5356 TODO #2
